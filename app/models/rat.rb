f2373f1f1c7abead5e867f924fa359be95e3ef54
Alastair Tooth <alastairtooth@gmail.com>
2021-08-31 12:31:01 +1000

    create the login functionality through devise

--- app/models/rat.rb
+++ app/models/rat.rb
@@ -1,9 +1,13 @@
 class Rat < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :bookings
   has_one_attached :photo
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  rat = Rat.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: "rat#{Faker::Internet.free_email}"
+  )
+  rat.save!
+end
+
+rats = Rat.all
+
+rats.each do |rat|
+  rand(4..10).times do
+    hat = Hat.new(
+      title: Faker::Name.first_name,
+      description: Faker::Name.last_name,
+      price: "rat#{Faker::Internet.free_email}",
+      size: rand("S", "M", "L"),
+      available: true,
+      rats_id: rat.id,
+      created_at: Faker::Time.between(from: DateTime.now - 5, to: DateTime.now),
+      updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+    )
+    hat.save!
+  end
+end
