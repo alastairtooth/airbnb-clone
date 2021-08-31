056932fd3d635f976a55dcd24871325c08dce018
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-31 12:27:58 +1000

    seed file

--- app/models/rat.rb
+++ app/models/rat.rb
@@ -5,5 +5,5 @@ class Rat < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
