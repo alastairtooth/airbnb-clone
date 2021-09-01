6bec6076d7cdca9162a447c6b68360a1891ddf09
Alastair Tooth <alastairtooth@gmail.com>
2021-09-01 15:48:06 +1000

    updated navbar functionality and homepage

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,15 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      console.log(window.scrollY)
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
