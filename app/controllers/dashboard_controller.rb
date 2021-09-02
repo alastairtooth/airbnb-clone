dc71f104f07e5c584d1ec7f6f581ba6e26301061
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-09-02 13:08:45 +1000

    dashboard scss and profile scss

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     @sent_bookings = current_rat.bookings
     # @bookings_recd = Booking.where(current_rat.hats)
     @hats = current_rat.hats
-    @pending_bookings = Booking.where(status: 'pending')
-    @historical_bookings = Booking.where(status: ['accepted', 'declined'])
+    @pending_bookings = Booking.where(status: 'pending', hat: current_rat.hats)
+    @historical_bookings = Booking.where(status: ['accepted', 'declined'], hat: current_rat.hats)
   end
 end
